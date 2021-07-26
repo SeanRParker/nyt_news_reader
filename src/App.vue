bfeef0d59ec7cc19a257a6170350558e0eb178f5
Shaws <seanrichardparker@gmail.com>
2021-07-26 13:05:12 -0400

    change favicon, format dates, populate cards with content

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="bg-gray-300 pb-5">
     <NewsFeed />
   </div>
 </template>
@@ -20,8 +20,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/NewsFeed/index.vue
+++ src/components/NewsFeed/index.vue
@@ -1,9 +1,14 @@
 <template>
-  <div id="news-feed">
-    <h1>newsFeed</h1>
+  <div id="news-feed" >
+    <h1 class="text-center text-2xl py-4">newsFeed</h1>
     <!-- <div v-for="story in this.articles" :key="story"> -->
-    <div v-for="story in this.articles" :key="story.slug_name">
-      <h3>{{story.title}}</h3>
+    <div v-for="(story, ind) in this.articles" :key="ind" class="shadow-lg rounded-lg bg-white p-6 mb-6">
+      <img class="pr-2 pb-2 inline-block w-1/5" :src=story.thumbnail_standard />
+      <div class="inline-block w-4/5">
+        <p>{{formatDate(story.published_date)}}</p>
+        <p><a :href="story.url" target="_blank">{{story.title}}</a></p>
+      </div>
+      <p class>{{story.abstract}}</p>
     </div>
   </div>
 </template>
@@ -27,7 +32,15 @@ export default {
         console.log(this.articles);
       })
       .catch(err => console.log(err));
-    }
+    },
+    formatDate(timestamp) {
+      let day = timestamp.split('T')[0];
+      let mm = day.split('-')[1];
+      let dd = day.split('-')[2];
+      let yyyy = day.split('-')[0];
+
+      return `${mm}-${dd}-${yyyy}`
+    },
   },
   created: function() {
     this.loadArticles();
@@ -37,6 +50,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#news-feed {
+  margin: 0 auto;
+  width: 80%;
+}
 h3 {
   margin: 40px 0 0;
 }
