e3e580cf7a8f0923372d4919130755e6196d78e8
Shaws <seanrichardparker@gmail.com>
2021-07-30 11:57:33 -0400

    add limit dropdown to call more posts from API

--- src/components/NewsFeed/index.vue
+++ src/components/NewsFeed/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="news-feed" class="sm:w-full md:w-4/5 my-0 mx-auto">
     <h1 class="text-center text-2xl py-4">newsFeed</h1>
+          <div>
+        <select @change="pickLimit($event)">
+          <option v-for="(num, ind) in limits" :value="num" :key="ind" >{{num}}</option>
+
+        </select>
+      </div>
       <div class="grid grid-flow-row sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         <div v-for="(story, ind) in this.articles" :key="ind" class=" shadow-lg rounded-lg bg-white gap-1.5 p-6 mb-4 align-top">
           <div class="flex card-heading">
@@ -12,11 +18,11 @@
             </div>
           </div>
           <p v-if="story.abstract">{{story.abstract}}</p>
-          <p v-else><b>Abstract not available, click link above to see story</b></p>
+          <p v-else><b>Summary not available, click above to see full story</b></p>
         </div>
       </div>
     <div id="footer" class="sticky bottom-0 bg-white">
-      <button v-for="(section, i) in sections" :key="i" @click="loadArticles(section)" class="inline-block w-1/4 py-4">{{section}}</button>
+      <button v-for="(section, i) in sections" :key="i" @click="loadArticles(section, currentLimit)" class="inline-block w-1/4 py-4">{{section}}</button>
     </div>
   </div>
 </template>
@@ -29,7 +35,10 @@ export default {
       BASE_URL: "https://api.nytimes.com/svc/news/v3/content/",
       apiKey: "42E7VHsQarerg6mapRfsNc4hwcWmP3QH",
       articles: [],
+      currentSection: 'all',
       sections: ['all', 'world', 'business', 'arts'],
+      limits: [],
+      currentLimit: 50
       // https://api.nytimes.com/svc/news/v3/content/all/all.json?limit=500&api-key=[YOUR_API_KEY]
       // sections: [{
       //   ""
@@ -37,8 +46,8 @@ export default {
     };
   },
   methods: {
-    loadArticles(section) {
-      fetch(this.BASE_URL + `all/${section}.json?limit=50&api-key=` + this.apiKey)
+    loadArticles(section, limit) {
+      fetch(this.BASE_URL + `all/${section}.json?$limit=${limit}&api-key=` + this.apiKey)
       .then(response => response.json())
       .then(data => {
         this.articles = data.results;
@@ -46,6 +55,7 @@ export default {
       })
       .catch(err => console.log(err));
       scroll(0,0);
+      this.currentSection = section;
     },
     formatDate(timestamp) {
       let day = timestamp.split('T')[0];
@@ -55,9 +65,18 @@ export default {
 
       return `${mm}-${dd}-${yyyy}`
     },
+    createLimitAmounts() {
+      for(let i = 50; i <= 500; i += 50) {
+        this.limits.push(i)
+      }
+    },
+    pickLimit(event) {
+      this.loadArticles(this.currentSection, event.target.value);
+    }
   },
   created: function() {
-    this.loadArticles('all');
+    this.createLimitAmounts();
+    this.loadArticles('all', this.currentLimit);
   }
 }
 </script>
